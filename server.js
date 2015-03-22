ec76f521ea76eecdf78d15ec186dc8827f375bc2
venkat <venkat.jaa@gmail.com>
2015-03-22 17:33:19 +0530

    serving static files

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 var express = require('express');
+var path = require('path');
 var app = express();
-app.get('/', function(req, res){
-	res.end("This is my first program using express for this repository, I get these examples from nodeschool expressworks");
-});
+app.use(express.static(process.argv[3]||path.join(__dirname, 'public')));
 app.listen(process.argv[2]);
 console.log("Node Server is listening on " + process.argv[2]);
